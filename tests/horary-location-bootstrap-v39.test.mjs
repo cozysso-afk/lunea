d2198d8c5779df5f66150543ae354765c8e40fda
cozysso-afk <cozysso@gmail.com>
2026-09-05 17:54:25 +0900

    chore(horary): remove obsolete bootstrap test

--- tests/horary-location-bootstrap-v39.test.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'node:fs';
-import assert from 'node:assert/strict';
-
-const loader = fs.readFileSync('lunea-horary-location-loader-v39.js', 'utf8');
-assert.match(loader, /lunea-horary-location-button-v39\.js/);
-console.log('horary location loader v39 contract ok');
